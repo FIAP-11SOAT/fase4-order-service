2d3de82336122449ac65f91833cc77b7077d5817
Matheus Ribeiro <teteuoliveira12@gmail.com>
2025-12-17 21:28:00 -0300

    feat(terraform): Atualiza configurações e adiciona segredos
    - Descomentei recursos de gerenciamento de segredos no Terraform em db-settings.tf, github-secrets.tf, secrets.tf e variables.tf.
    - Adicionei a configuração do AWS Secrets Manager no pom.xml para gerenciar segredos de forma mais eficiente.
    - Atualizei a classe RestTemplateConfig.java para incluir um espaço em branco no final.
    - Adicionei a variável JWT_JWK na classe HealthController.java para permitir a injeção de dependência.
    - Atualizei application.properties para importar segredos do AWS Secrets Manager.
    
    Essas mudanças melhoram a segurança do projeto ao integrar o gerenciamento de segredos e facilitam a configuração do ambiente, permitindo que as credenciais sensíveis sejam gerenciadas de forma mais segura e eficiente.

--- deploy/terraform/secrets.tf
+++ deploy/terraform/secrets.tf
@@ -1,14 +1,18 @@
-# resource "aws_secretsmanager_secret" "secrets" {
-#   name                    = "${local.project_name}-secrets"
-#   description             = "Secrets for ${local.project_name} project"
-#   recovery_window_in_days = 0
+resource "aws_secretsmanager_secret" "secrets" {
+  name                    = "${local.project_name}-secrets"
+  description             = "Secrets for ${local.project_name} project"
+  recovery_window_in_days = 0
 
-#   tags = {
-#     Name = "${local.project_name}-secrets"
-#   }
-# }
+  tags = {
+    Name = "${local.project_name}-secrets"
+  }
+}
 
-# resource "aws_secretsmanager_secret_version" "secrets" {
-#   secret_id     = aws_secretsmanager_secret.secrets.id
-#   secret_string = jsonencode({})
-# }
+resource "aws_secretsmanager_secret_version" "secrets" {
+  secret_id     = aws_secretsmanager_secret.secrets.id
+  secret_string = jsonencode({
+    "spring.datasource.url"      = "jdbc:postgresql://localhost:5432/default",
+    "spring.datasource.username" = "default",
+    "spring.datasource.password" = "default",
+  })
+}
--- src/main/java/com/fiap/soat11/order/controller/HealthController.java
+++ src/main/java/com/fiap/soat11/order/controller/HealthController.java
@@ -1,5 +1,6 @@
 package com.fiap.soat11.order.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class HealthController {
     
+
+    @Value("${JWT_JWK}")
+    private String JWT_JWK;
+
     @GetMapping
     public String status() {
         return "Order Service is running";
